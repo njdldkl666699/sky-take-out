379d117ddb1a0919fccb4674dbea4848a36c852c
南极大陆的可乐 <zhangle666699@qq.com>
2025-04-23 12:42:15 +0800

    初始化工程，修复knife4j版本问题；修复druid版本问题；修复jjwt版本问题，修改HS256密钥至32位以上，以解决弱密钥问题

---
+++ sky-server/src/main/java/com/sky/config/Knife4jConfiguration.java
@@ -0,0 +1,31 @@
+package com.sky.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，注册Knife4j相关组件
+ */
+@Configuration
+@Slf4j
+public class Knife4jConfiguration {
+
+    /**
+     * 设置基本信息
+     * @return
+     */
+    @Bean
+    public OpenAPI openAPI() {
+        log.info("开始注册Knife4j...");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("苍穹外卖项目接口文档")
+                        .version("2.0")
+                        .description("苍穹外卖项目接口文档"));
+    }
+}
