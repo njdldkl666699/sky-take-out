00d330203fc0ece8bd97a2d5dd338d4c58fb9fc5
南极大陆的可乐 <zhangle666699@qq.com>
2025-05-02 15:52:07 +0800

    用户下单代码开发

--- sky-pojo/src/main/java/com/sky/dto/OrdersSubmitDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/OrdersSubmitDTO.java
@@ -1,6 +1,7 @@
 package com.sky.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,24 +9,34 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@Schema(title = "用户下单DTO")
 public class OrdersSubmitDTO implements Serializable {
-    //地址簿id
+
+    @Schema(title = "地址簿id")
     private Long addressBookId;
-    //付款方式
+
+    @Schema(title = "付款方式")
     private int payMethod;
-    //备注
+
+    @Schema(title = "备注")
     private String remark;
-    //预计送达时间
+
+    @Schema(title = "预计送达时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime estimatedDeliveryTime;
-    //配送状态  1立即送出  0选择具体时间
+
+    @Schema(title = "配送状态", description = "1立即送出 0选择具体时间")
     private Integer deliveryStatus;
-    //餐具数量
+
+    @Schema(title = "餐具数量")
     private Integer tablewareNumber;
-    //餐具数量状态  1按餐量提供  0选择具体数量
+
+    @Schema(title = "餐具数量状态", description = "1按餐量提供  0选择具体数量")
     private Integer tablewareStatus;
-    //打包费
+
+    @Schema(title = "打包费")
     private Integer packAmount;
-    //总金额
+
+    @Schema(title = "总金额")
     private BigDecimal amount;
 }
---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.sky.mapper;
+
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderMapper {
+
+    /**
+     * 向订单表插入数据
+     *
+     * @param order
+     */
+    void insert(Orders order);
+}
